fa7b46fce5a98769a047ae6a1be0e38c4294e7fa
Katia Ricky <kricky@ev-a1.kzn.21-school.ru>
2021-05-21 15:17:21 +0300

    init

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+void	ft_strcpy(char *dst, const char *src);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+#endif
